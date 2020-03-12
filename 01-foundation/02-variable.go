ff4c6ce0573dfe6d0f412a406407624e2f6ecaba
phoenixfate <sm516116978@outlook.com>
2020-03-13 00:58:33 +0800

    Initial commit

---
+++ 01-foundation/02-variable.go
@@ -0,0 +1,33 @@
+package main
+
+//导入包 必须使用，否则报错
+import "fmt"
+
+func main() {
+
+	//1. 声明 var 变量名 类型；
+	//变量声明了必须使用，否则报错
+	//2. 只是声明没有初始化的变量，默认0
+	var a int
+
+	fmt.Println("a = ", a)
+
+	//4.声明多个变量
+	var b, c int
+	c = 20
+	fmt.Printf("b= %d, c= %d\n", b, c)
+	fmt.Printf("xxx\n")
+
+	//5.变量初始化
+	var d int = 30
+	fmt.Println(d)
+
+	//6.自动推导类型，必须初始化   !!!!!!!!!!!!!!!!!!!!!!!!
+	e := 50
+	//%T 变量所属的类型
+	fmt.Printf("c type is %T\n", e)
+
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# 个人学习使用
+## 编译运行
+- go build xxx.go
+- go run xxx.go
