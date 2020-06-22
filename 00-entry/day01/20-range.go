679ff0493728aa0b952a8b5e8908ef2a47b57032
phoenix fate <sm516116978@outlook.com>
2020-06-22 20:30:02 +0800

    go基础进阶 day02

--- 00-entry/day01/20-range.go
+++ 00-entry/day01/20-range.go
@@ -6,8 +6,8 @@ func main() {
 	//range用于迭代遍历
 	str := "abc"
 	//range 返回两个参数，第一个是元素的位置，一个是元素本身
-	for i, data := range str {
-		fmt.Printf("str[%d]=%c\n", i, data)
+	for position, char := range str {
+		fmt.Printf("str[%d]=%c\n", position, char)
 	}
 
 	arr := []int{12, 22, 32, 42}
