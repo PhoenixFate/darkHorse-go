a401cabad99ac4f67be788810ced54c22e86e693
phoenixfate <sm516116978@outlook.com>
2020-06-29 00:54:26 +0800

    区块链v3 添加命令行

--- 04-04-betterBlockChainV3/main.go
+++ 04-04-betterBlockChainV3/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 //mac/linux 下面
 //cd 04-00-block-chain/04-01-firstBlockChain
 //go run *.go
@@ -13,28 +11,8 @@ import "fmt"
 
 func main() {
 	blockChain := NewBlockChain()
-	blockChain.AddBlock("班长向班花转了50枚比特币")
-
-	it := blockChain.NewIterator()
-	//调用迭代器
-	for {
-		//Next 返回当前区块，并且指针左移
-		block := it.Next()
-
-		fmt.Printf("===========================\n")
-		fmt.Printf("当前区块哈希值： %x\n", block.Hash)
-		fmt.Printf("前区块哈希值：%x\n", block.PrevHash)
-		fmt.Printf("区块数据： %s\n", block.Data)
-		fmt.Printf("当前区块随机数: %d\n", block.Nonce)
-		fmt.Printf("当前区块难度值: %d\n", block.Difficulty)
-		fmt.Printf("===========================\n\n")
-
-		if len(block.PrevHash) == 0 {
-			fmt.Println("区块链编译结束")
-			break
-		}
-	}
-
+	commandLine := CommandLine{blockChain}
+	commandLine.Run()
 	//time.Sleep(time.Second * 100)
 
 }
